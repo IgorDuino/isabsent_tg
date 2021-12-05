dbaf956739df603fb34906c88e03791d1af79dfc
Igor <74809945+IgorDuino@users.noreply.github.com>
2021-12-05 20:33:13 +0300

    Update tools.py
    add validate_date

--- tools.py
+++ tools.py
@@ -1,5 +1,14 @@
 def validate_date(date: str):
-    return True
+    try:
+        data = data.split('-')
+        if len(data) == 3:
+            if len(data[0]) == 4 and data[0].isdigit():
+                if len(data[1]) < 3 and data[1].isdigit():
+                    if len(data[2]) < 3 and data[2].isdigit():
+                        return True
+    except:
+        pass
+    return False
 
 
 def translate_reason(reason: str):
